484c961741153a9b755fdc5fa72543ea769f0e9e
hzweifei <2710175658@qq.com>
2024-07-18 23:27:40 +0800

    增加笑点

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ img_dir = "image/chessboard"
 pattern_type = PatternInfo(PatternType.CHESSBOARD, (19, 17), 0.5, (4.8,4.8))
 camera = Calibrator(img_dir,pattern_type,0.4)
 camera.calibrate_camera()
+print("LOL")
 
